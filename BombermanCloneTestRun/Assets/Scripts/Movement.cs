7c671349ea427826c1afb4e71efd0cb8df30d859
Felix Curran <42895833+FelixCurranPAL@users.noreply.github.com>
2018-09-01 22:27:04 +0100

    Added basic character movement system
    Created player and movement system. Character can move and not go through walls, but only for this exact map setup.

---
+++ BombermanCloneTestRun/Assets/Scripts/Movement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    float lastStep, timeBetweenSteps = 0.05f;
+    public float movementSpeed = 0.25f;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        var pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            if (pos.y <= 3.75)
+            {
+                if (Time.time - lastStep > timeBetweenSteps)
+                {
+                    lastStep = Time.time;
+                    pos.y += movementSpeed;
+                    transform.position = pos;
+                }
+            }
+        }
+
+        if (Input.GetKey("a"))
+        {
+            if (pos.x >= -3.75)
+            {
+                if (Time.time - lastStep > timeBetweenSteps)
+                {
+                    lastStep = Time.time;
+                    pos.x -= movementSpeed;
+                    transform.position = pos;
+                }
+            }
+        }
+
+        if (Input.GetKey("s"))
+        {
+            if (pos.y >= -2.75)
+            {
+                if (Time.time - lastStep > timeBetweenSteps)
+                {
+                    lastStep = Time.time;
+                    pos.y -= movementSpeed;
+                    transform.position = pos;
+                }
+            }
+        }
+
+        if (Input.GetKey("d"))
+        {
+            if (pos.x <= 3.75)
+            {
+                if (Time.time - lastStep > timeBetweenSteps)
+                {
+                    lastStep = Time.time;
+                    pos.x += movementSpeed;
+                    transform.position = pos;
+                }
+            }
+        }
+    }
+}
