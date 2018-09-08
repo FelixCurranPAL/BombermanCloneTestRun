120bc19e629eb43d556ac10fd9b2a20defb7eeac
Felix Curran <42895833+FelixCurranPAL@users.noreply.github.com>
2018-09-08 02:44:15 +0100

    Found the correct way to handle player movement, still needs tweaked
    Removed transform and lerp method of moving character and replaced with physics based AddForce and Velocity method. Works as we want it to, but needs some tweaking when pressing multiple buttons.
    
    Corrected bomb script to work with new movement system.
    
    Modified Tile Palette and Tilemap Grid to remove any overlap between tiles.

---
+++ BombermanCloneTestRun/Assets/Scripts/Bomb.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+    public int bombtime = 5;
+
+    void Start ()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Destroy(gameObject, bombtime);
+    }
+
+    void OnTriggerExit2D ()
+    {
+        gameObject.GetComponent<Collider2D>().isTrigger =  false;
+    }
+
+}
--- BombermanCloneTestRun/Assets/Scripts/Movement.cs
+++ BombermanCloneTestRun/Assets/Scripts/Movement.cs
@@ -3,23 +3,17 @@ using UnityEngine;
 using System.Timers;
 using System;
 
-using UnityEngine;
-using System.Timers;
-using System;
+//using UnityEngine;
+//using System.Timers;
+//using System;
 
 public class Movement : MonoBehaviour
 {
-    float lastStep, timeBetweenSteps = 0.05f;
-
-    float lastFrame, timeBetweenFrames = 0.25f;
-    int animationStep = 0;
-
-    public float movementSpeed = 0.25f;
     public GameObject bombPrefab;
-    public Sprite DownIdle, LeftIdle, RightIdle, UpIdle, DownMove1, LeftMove1, RightMove1, UpMove1, DownMove2, LeftMove2, RightMove2, UpMove2;
-    public int bombStrength;
-    public int bombDuration;
-    Transform GetTransform;
+    public float speed;
+
+    private bool moving;
+    private Rigidbody2D characterRigidbody;
 
 
     // Use this for initialization
@@ -31,138 +25,47 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement();
         placeBombs();
+    }
+
+    // Used for all physics based interactions
+    void FixedUpdate()
+    {
+        movement();
     }
 
     void movement()
     {
-        var pos = transform.position;
+        characterRigidbody = GetComponent<Rigidbody2D>();
 
         if (Input.GetKey("w"))
         {
-
-            if (Time.time - lastStep > timeBetweenSteps)
-            {
-                lastStep = Time.time;
-                pos.y += movementSpeed;
-                transform.position = pos; // TO DO: NEED TO REPLACE WITH GRADUAL MOVEMENT SYSTEM
-            }
-
-
-            if (animationStep < 1)
-            {
-                if (Time.time - lastFrame > timeBetweenFrames)
-                {
-                    this.GetComponent<SpriteRenderer>().sprite = UpMove1;
-                    animationStep = animationStep + 1;
-                    lastFrame = Time.time;
-                }
-            }
-            else if (animationStep == 1)
-            {
-                if (Time.time - lastFrame > timeBetweenFrames)
-                {
-                    this.GetComponent<SpriteRenderer>().sprite = UpMove2;
-                    animationStep = 0;
-                    lastFrame = Time.time;
-                }
-
-
-            }
+            characterRigidbody.AddForce(transform.up * speed);
+            moving = true;
         }
 
         if (Input.GetKey("a"))
         {
-
-            if (Time.time - lastStep > timeBetweenSteps)
-            {
-                lastStep = Time.time;
-                pos.x -= movementSpeed;
-                transform.position = pos;
-            }
-
-
-            if (animationStep < 1)
-            {
-                if (Time.time - lastFrame > timeBetweenFrames)
-                {
-                    this.GetComponent<SpriteRenderer>().sprite = LeftMove1;
-                    animationStep = animationStep + 1;
-                    lastFrame = Time.time;
-                }
-            }
-            else if (animationStep == 1)
-            {
-                if (Time.time - lastFrame > timeBetweenFrames)
-                {
-                    this.GetComponent<SpriteRenderer>().sprite = LeftMove2;
-                    animationStep = 0;
-                    lastFrame = Time.time;
-                }
-            }
+            characterRigidbody.AddForce(-transform.right * speed);
+            moving = true;
         }
 
         if (Input.GetKey("s"))
         {
-
-            if (Time.time - lastStep > timeBetweenSteps)
-            {
-                lastStep = Time.time;
-                pos.y -= movementSpeed;
-                transform.position = pos;
-            }
-
-
-            if (animationStep < 1)
-            {
-                if (Time.time - lastFrame > timeBetweenFrames)
-                {
-                    this.GetComponent<SpriteRenderer>().sprite = DownMove1;
-                    animationStep = animationStep + 1;
-                    lastFrame = Time.time;
-                }
-            }
-            else if (animationStep == 1)
-            {
-                if (Time.time - lastFrame > timeBetweenFrames)
-                {
-                    this.GetComponent<SpriteRenderer>().sprite = DownMove2;
-                    animationStep = 0;
-                    lastFrame = Time.time;
-                }
-            }
+            characterRigidbody.AddForce(-transform.up * speed);
+            moving = true;
         }
 
         if (Input.GetKey("d"))
         {
+            characterRigidbody.AddForce(transform.right * speed);
+            moving = true;
+        }
 
-            if (Time.time - lastStep > timeBetweenSteps)
-            {
-                lastStep = Time.time;
-                pos.x += movementSpeed;
-                transform.position = pos;
-            }
-
-
-            if (animationStep < 1)
-            {
-                if (Time.time - lastFrame > timeBetweenFrames)
-                {
-                    this.GetComponent<SpriteRenderer>().sprite = RightMove1;
-                    animationStep = animationStep + 1;
-                    lastFrame = Time.time;
-                }
-            }
-            else if (animationStep == 1)
-            {
-                if (Time.time - lastFrame > timeBetweenFrames)
-                {
-                    this.GetComponent<SpriteRenderer>().sprite = RightMove2;
-                    animationStep = 0;
-                    lastFrame = Time.time;
-                }
-            }
+        if (moving == true && (!Input.GetKey("w") && !Input.GetKey("a") && !Input.GetKey("s") && !Input.GetKey("d")))
+        {
+            characterRigidbody.velocity = Vector2.zero;
+            moving = false;
         }
     }
 
